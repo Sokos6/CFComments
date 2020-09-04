4b2ee57e4fd1fb153ed71d924243557a7e551b72
Sokos6 <will.sokolowski@gmail.com>
2020-09-04 16:37:35 -0500

    follow the commits, how do you refactor now?

--- src/index.js
+++ src/index.js
@@ -1,21 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
   return (
-    <div className='ui container comments'>
-      <div className='comment'>
-        <a href='/' className='avatar'>
-          <img alt='avatar' />
-        </a>
-        <div className='content'>
-          <a href='/' className='author'>
-            Will
+    <div>
+      <div className='ui container comments'>
+        <div className='comment'>
+          <a href='/' className='avatar'>
+            <img alt='avatar' src={faker.image.avatar()} />
           </a>
-          <div className='metadata'>
-            <span className='date'>Today at 3pm</span>
+          <div className='content'>
+            <a href='/' className='author'>
+              Will
+            </a>
+            <div className='metadata'>
+              <span className='date'>Today at 3pm</span>
+            </div>
+            <div className='test'>Great Post</div>
           </div>
-          <div className='test'>Great Post</div>
+        </div>
+      </div>
+      <div className='ui container comments'>
+        <div className='comment'>
+          <a href='/' className='avatar'>
+            <img alt='avatar' src={faker.image.avatar()} />
+          </a>
+          <div className='content'>
+            <a href='/' className='author'>
+              Will
+            </a>
+            <div className='metadata'>
+              <span className='date'>Today at 3pm</span>
+            </div>
+            <div className='test'>Great Post</div>
+          </div>
+        </div>
+      </div>
+      <div className='ui container comments'>
+        <div className='comment'>
+          <a href='/' className='avatar'>
+            <img alt='avatar' src={faker.image.avatar()} />
+          </a>
+          <div className='content'>
+            <a href='/' className='author'>
+              Will
+            </a>
+            <div className='metadata'>
+              <span className='date'>Today at 3pm</span>
+            </div>
+            <div className='test'>Great Post</div>
+          </div>
         </div>
       </div>
     </div>
