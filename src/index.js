6552c7eec151d96143b8067d65af7565afc119d5
Sokos6 <will.sokolowski@gmail.com>
2020-09-04 16:28:29 -0500

    initial jsx for refactor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div> Hi There!</div>;
+  return (
+    <div className='ui container comments'>
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img alt='avatar' />
+        </a>
+        <div className='content'>
+          <a href='/' className='author'>
+            Will
+          </a>
+          <div className='metadata'>
+            <span className='date'>Today at 3pm</span>
+          </div>
+          <div className='test'>Great Post</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
